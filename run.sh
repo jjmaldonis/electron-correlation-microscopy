844e120949c5fd004fc0cd68e77c85e2d461e64e
Jason Maldonis <maldonis@submit-3.chtc.wisc.edu>
2017-02-28 13:44:26 -0600

    adding files for T = 238 C

--- run.sh
+++ run.sh
@@ -3,28 +3,19 @@
 ls -l
 echo $@
 
-#wget http://proxy.chtc.wisc.edu/SQUID/maldonis/anaconda2.tar.gz
 wget -q http://proxy.chtc.wisc.edu/SQUID/maldonis/python.tar.gz
-wget -q http://proxy.chtc.wisc.edu/SQUID/maldonis/tif_C234.tar.gz
+wget -q http://proxy.chtc.wisc.edu/SQUID/maldonis/tif_C238.tar.gz
 
 # untar your Python installation and other necessary files
-#tar -xf anaconda2.tar.gz
 tar -xzf python.tar.gz
-tar -xzf tif_C234.tar.gz
+tar -xzf tif_C238.tar.gz
 
 #rm anaconda2.tar.gz
 rm python.tar.gz
-rm tif_C234.tar.gz
+rm tif_C238.tar.gz
 
 # make sure the script will use your Python installation
-#export PATH=$(pwd)/anaconda2/bin:$PATH
 export PATH=$(pwd)/python/bin:$PATH
-
-#wget https://bootstrap.pypa.io/get-pip.py
-#python get-pip.py
-#pip install conda
-#conda create -n py3 --file exported.txt
-#source activate py3
 
 # Setup for script
 mkdir spatial_results
@@ -44,7 +35,4 @@ cd ../
 
 # run your script
 python g4spatial_unaveraged.py $@
-# The above command should create an (x,y).data image
-
-#rm -rf python tif R2xy_data  # Since these are dirs, they won't be transfered back anyways
 
--- utilities.py
+++ utilities.py
@@ -16,10 +16,9 @@ def load_matplotlib():
 def load_pixel_positions(filename=None):
     if filename is None:
         head, tail = os.path.split(__file__)
-        filename = os.path.join(head, "nanowire_background_C234.txt")
+        filename = os.path.join(head, "nanowire_background_C238.txt")
     mask = np.loadtxt(filename, skiprows=1)
     mask = np.rot90(np.fliplr(mask))  # Correct for the mask slightly, see notebook title "Aligning the Mask with the Tifs"
-    mask = mask[:, 2:]  # For C234
     pixel_positions = list(zip(*np.where(mask == 255)))
     return pixel_positions
 
@@ -29,12 +28,12 @@ def load(fn):
 
 
 def load_tif_data(nframes, nprocs=4):
-    fns = ["tif/C234_2s_{i}.tif".format(i=str(i).zfill(4)) for i in range(nframes)]
+    fns = ["tif/C238_{i}.tif".format(i=str(i).zfill(4)) for i in range(nframes)]
 
-    data = np.zeros((208, 198, nframes), dtype=np.float)
+    data = np.zeros((210, 221, nframes), dtype=np.float)
 
     for i, fn in enumerate(fns):
-        data[:,:,i] = load(fn)
+        data[:,:,i] = load(fn)[:, :-1]  # For C238
     print("Finished loading data!")
 
     return data
