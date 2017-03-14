overhaul to g4spatial calculation; it should be correct now

create_R_data.py
@@ -1,20 +1,74 @@
+from __future__ import print_function
 import sys
+import json
 import time
 import numpy as np
-from utilities import load_pixel_positions
-from utilities import load_R_pixel_positions
+from utilities import load_pixel_positions, load_mask, load_tif_data, load_R_pixel_positions
 
 
 
 def main():
-    pixel_positions = load_pixel_positions()
+    paramfile = sys.argv[1]
+    params = json.load(open(paramfile))
+
+    nframes = params["nframes"]
+    dt_range = params["dT range"]
+    dr_range = params["dR range"]
+    tsize = dt_range[1] - dt_range[0]
+    rsize = dr_range[1] - dr_range[0]
+    rscale = params["Rscale"]
+    tscale = params["Tscale"]
+
+    mask = load_mask(dataset_key=params["dataset_key"], rotate_mask=params["rotate mask"],
+                    )
+    print("Loaded mask.")
+    pixel_positions = load_pixel_positions(mask)
+    print("Loaded masked positions.")
+
+    data = load_tif_data(nframes,
+                         #xsize=params["image size"][0], ysize=params["image size"][1],
+                         filename_base=params["tif_filename_base"],
+                        )
+    np.save("dtdata.npy", data)
+    print(data.shape)
+    print("Loaded tif data.")
+    print("Min intensity of tifs:", np.nanmin(data))
+    print("Max intensity of tifs:", np.nanmax(data))
+
+    drshape = tuple(list(data.shape) + [dr_range[1]-dr_range[0]])
+    print(drshape)
+    #drdata = np.zeros(drshape, dtype=np.uint64)
+    drdata = np.zeros(drshape, dtype=np.float64)
+    print("Created empty data.")
+    drdata.fill(np.nan)
+    print("Filled with nans.")
+    drdata[:, :, :, 0] = data
+    print("Set initial data.")
+    assert (drdata[:, :, :, 0] == data).all()
+    #assert (drdata[:, :, :, 1:] == np.nan).all()
+    assert len(np.where(~np.isnan(drdata[:, :, :, 1:]))[0]) == 0
+    print("Ran checks on initial data.")
+    np.save("initialized_drdata.npy", drdata)
+    print("Saved initial data.")
+
 
     width = 1.0
-    R = sys.argv[1]
-    R = float(R)
+    #R = sys.argv[1]
+    #R = float(R)
 
-    R_pixel_positions = load_R_pixel_positions(pixel_positions, R, width)
+    for R in range(*dr_range):
+        R = float(R)
+        print("dR: {}".format(R))
+        R_pixel_positions = load_R_pixel_positions(pixel_positions, R, width)
+        print("Finished loading R_pixel_positions")
+        for t in range(drdata.shape[0]):
+            print("t: {}".format(t), end="; ")
+            for (R,x,y), positions in R_pixel_positions.items():
+                drdata[t, x, y, int(R)] = np.mean([data[t, x, y] for x,y in positions])
+            print(t, x, y, int(R), drdata[t, x, y, int(R)])
 
+    np.save("drdata.npy", drdata)
 
 if __name__ == "__main__":
     main()
+

utilities.py
@@ -13,13 +13,22 @@ def load_matplotlib():
     import matplotlib.image as mpimg
 
 
-def load_pixel_positions(filename=None):
+def load_mask(filename=None, dataset_key=None, rotate_mask=True, xstart=0, xend=None, ystart=0, yend=None):
     if filename is None:
         head, tail = os.path.split(__file__)
-        filename = os.path.join(head, "nanowire_background_C238_2.txt")
+        filename = os.path.join(head, "masks/mask_{key}.txt".format(key=dataset_key))
     mask = np.loadtxt(filename, skiprows=1)
     mask = np.rot90(np.fliplr(mask))
-    pixel_positions = list(zip(*np.where(mask == 255)))
+    if xend is None:
+        xend = mask.shape[0]
+    if yend is None:
+        yend = mask.shape[1]
+    mask = mask[xstart:xend, ystart:yend]
+    return mask
+
+
+def load_pixel_positions(mask):
+    pixel_positions = list(zip(*np.where(mask == 0)))
     return pixel_positions
 
 
@@ -27,13 +36,24 @@ def load(fn):
     return np.array(Image.open(fn))
 
 
-def load_tif_data(nframes, nprocs=4):
-    fns = ["tif/C238_1s_{i}.tif".format(i=str(i).zfill(4)) for i in range(nframes)]
+#def load_tif_data(nframes, xsize, ysize, filename_base, xstart=0, xend=None, ystart=0, yend=None):
+def load_tif_data(nframes, filename_base, xstart=0, xend=None, ystart=0, yend=None):
+    fns = ["tif/{base}_{i}.tif".format(i=str(i).zfill(4), base=filename_base) \
+           for i in range(nframes)]
 
-    data = np.zeros((230, 240, nframes), dtype=np.float)
+    # Load one tif to get the size
+    tif = load(fns[0])
+    xsize, ysize = tif.shape
+
+    if xend is None:
+        xend = xsize
+    if yend is None:
+        yend = ysize
+
+    data = np.zeros((xsize, ysize, nframes), dtype=np.float)
 
     for i, fn in enumerate(fns):
-        data[:,:,i] = load(fn)
+        data[:,:,i] = load(fn)[xstart:xend, ystart:yend]
     print("Finished loading data!")
 
     return data
@@ -63,5 +83,4 @@ def load_R_pixel_positions(pixel_positions, R, width):
 
     print("Successfully loaded R={}".format(R))
     return R_pixel_positions
-
 